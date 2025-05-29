ddadd9e669e9a6cb09f82b5eb945aaa8f67b5e44
Alicia <aliciatangweishan@gmail.com>
2025-05-29 22:22:53 +0800

    small minor edits to sql queries

--- backend/sample.sql
+++ backend/sample.sql
@@ -8,7 +8,7 @@ SET profiling = 1;
 CREATE DATABASE IF NOT EXISTS game_haven;
 USE game_haven;
 
-CREATE TABLE customers (
+CREATE TABLE IF NOT EXISTS customers (
   id INT AUTO_INCREMENT PRIMARY KEY,
   firstName VARCHAR(255) NOT NULL,
   lastName VARCHAR(255) NOT NULL,
@@ -19,6 +19,8 @@ CREATE TABLE customers (
   avatarUrl VARCHAR(255),
   createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+DROP TRIGGER IF EXISTS set_default_avatar_customers;
 
 CREATE TRIGGER set_default_avatar_customers
 BEFORE INSERT ON customers
@@ -32,14 +34,14 @@ SET NEW.avatarUrl =
 
 SELECT * FROM game_haven.customers;
 
--- DEFAULT PASSWORD: 'password123' UNHASHED 
+-- DEFAULT PASSWORD: 'password1234' UNHASHED 
 
 INSERT IGNORE INTO game_haven.customers (firstName, lastName, gender, contactNo, email, password, createdAt)
 VALUES 
 ('Wendy', 'Tan', 'F', '81232313', 'test@gmail.com', '$2b$10$vi4bWxn67DlbmL4KpqGaQeyL9NwH53j23DAhpCVz/SW75mLV1GuFO', CURRENT_TIMESTAMP),
 ('Richard', 'Lee', 'M', '94828873', 'test2@gmail.com', '$2b$10$vi4bWxn67DlbmL4KpqGaQeyL9NwH53j23DAhpCVz/SW75mLV1GuFO', CURRENT_TIMESTAMP);
 
-CREATE TABLE admin (
+CREATE TABLE IF NOT EXISTS admin (
   id INT AUTO_INCREMENT PRIMARY KEY,
   firstName VARCHAR(255) NOT NULL,
   lastName VARCHAR(255) NOT NULL,
@@ -48,9 +50,11 @@ CREATE TABLE admin (
   contactNo VARCHAR(10) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
-  avatarUrl VARCHAR(255) DEFAULT '/images/profile/user-1.jpg',
+  avatarUrl VARCHAR(255),
   createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+DROP TRIGGER IF EXISTS set_default_avatar_admin;
 
 CREATE TRIGGER set_default_avatar_admin
 BEFORE INSERT ON admin
